83d05dd6feb3cc50b31027f32e367aa0bf48d355
chicchicchic <64607059+chicchicchic@users.noreply.github.com>
2021-10-14 19:59:06 +0700

    Add source code, ...30:12

--- logger.js
+++ logger.js
@@ -1,1 +1,46 @@
 // File này có tác dụng để ghi log, ví dụ như trạng thái trước khi dispatch và sau khi dispatch của việt thêm item khi user nhập vào input và nhấn Enter thì 'onkeyup' sẽ bắt dc event dispatch action của User, file logger này sẽ cho ta thấy trang thái trước và sau khi action dc dispatch
+export default function logger(reducer) {
+    // return lại hàm khác
+    // trong hàm mà nó return thì nó cũng phải nhận những đối số giống reducer
+    // 'prevState' là state trc đó
+    return (prevState, action, args) => {
+        // nhóm lại theo action (console.group(action)), và end group (console.groupEnd()), khi này thì mình nhấn phím Enter là sẽ thấy nó log ra 'add' (là cái action mà ta đã bắt sự kiện bên attribute 'onkeyup' ở file Header.js)
+        console.group(action);
+
+        // ghi log 'prevState'
+        console.log('Prev State: ', prevState);
+
+        // ghi log 'Arguments'
+        console.log('Action Arguments: ', args);
+
+        // 'prevState' và 'nextState' chỉ là để ghi log lại state củ và state mới để mình nhìn thấy thôi
+        const nextState = reducer(prevState, action, args)
+
+        // ghi log 'nextState'
+        console.log('Next State: ', nextState);
+
+        // End group log 'action'
+        console.groupEnd()
+        return nextState
+
+        
+    }
+}
+
+// Khi ta vừa run lần đầu tiên nó hiện:
+/**
+ * undefined (là action name)
+    rev State: undefined
+    Action Arguments: undefined
+    Next State: {todos: Array(2)} (vì giá trị khởi tạo (init) của ta là 2 phần tử và ở bước Next State nó load giá trị khởi tạo của ta)
+ */
+
+
+// Khi ta nhập giá trị gì đó (Vd: join the meeting) vào thẻ input trên phần Header (nhập công việc cần làm) thì nó sẽ ghi lại log mới là: 
+/**
+ * add (là action name 'add')
+    Prev State: {todos: Array(2)} (nó lấy giá trị nextState của log củ làm giá trị preState của log mới)
+    Action Arguments: ['join the meeting'] (là giá trị User nhập vào và nhấn Enter)
+    Next State: {todos: Array(2)} (đáng lẽ nextState nó là '{todos: Array(3)}' vì dc User thêm 1 giá trị 'join the meeting' vào todos nhưng do ta chưa viết hàm 'add' để thêm vào nên nó vẫn hiện là '{todos: Array(2)}')
+ */
+
--- store.js
+++ store.js
@@ -1,10 +1,15 @@
 import { createStore } from "./core.js";
 // import thêm reducer nữa vì khi ta viêt createStore thì nó nhạn đối số là reducer
 import reducer from "./reducer.js";
+// import logger vào Store
+import withLogger from './logger.js'
 
 // createStore của ta lại trả ra 'attach, connect, dispatch' nên trong store.js lấy ra 'attach, connect, dispatch'
 // gọi createStore thì nó sẽ return 'attach, connect, dispatch'
-const { attach, connect, dispatch } = createStore(reducer)
+// vì 'logger' là thành phần trung gian nên nó ôm bên ngoài reducer 'withLogger(reducer)', khi reducer dc gọi mà withlogger (logger) nó ôm reducer như vậy thì nó sẽ bắt trạng thái của reducer.
+// vì reducer là hàm, withlogger củng là hàm nên bản thân trong withLogger cũng return lại 1 hàm
+// làm gì làm, mình vẫn phải viết 1 hàm mới ôm reducer mà nó trả ra đúng những cái createStore cần, khi mình ôm 1 hàm khác thì mình phải làm sao đảm báo là hàm đó dc gọi nó vẫn trả ra cái state 
+const { attach, connect, dispatch } = createStore(withLogger(reducer))
 
 // dispatch cần đạt biến global cho dê dùng, trong View cũng cần dispatch action nên cần đặt biến global
 window.dispatch = dispatch
